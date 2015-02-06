56ac9332d4561c417087445e78d2dfe90c95aec4
EricSchles <Ericschles@gmail.com>
2015-02-06 16:21:34 -0500

    Create dp_exercise.java

---
+++ java_primer/dp_exercise.java
@@ -0,0 +1,12 @@
+import java.io.*; //always import *
+import java.util.*;
+public class dp_exercise{
+    public static <T> void print(T input){
+	System.out.println(input);
+    }
+    public static void main(String[] args) throws Exception{
+        
+       print(fib(10));
+      // print(fib_better(10));
+    }
+}
